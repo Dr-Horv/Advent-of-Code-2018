7ecc686a314252a28380e4816c074048a1bd5f2f
Johan Horvius <drhorv@gmail.com>
2018-12-05 23:20:57 +0100

    Solution for Day5

--- cmd/aoc.go
+++ cmd/aoc.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/Dr-Horv/Advent-of-Code-2018/internal/day04"
+	"github.com/Dr-Horv/Advent-of-Code-2018/internal/day05"
 	"github.com/Dr-Horv/Advent-of-Code-2018/internal/pkg"
 	"time"
 )
 
 func main() {
-	var day = "04"
+	var day = "05"
 	lines := pkg.ReadFile("./internal/day" + day + "/input")
 	start := time.Now()
-	answer := day04.Solve(lines, false)
+	answer := day05.Solve(lines, false)
 	t := time.Now()
 	elapsed := t.Sub(start)
 	fmt.Println(answer)
--- internal/day05/day05.go
+++ internal/day05/day05.go
@@ -1,34 +1,133 @@
 package day05
 
-import "unicode"
+import (
+	"fmt"
+	"math"
+	"strings"
+	"unicode"
+)
+
+type job func(rune) string
+
+func worker(id int, jobs <-chan rune, results chan<- string, job job) {
+	for j := range jobs {
+		result := job(j)
+		results <- result
+	}
+}
 
 func Solve(lines []string, partOne bool) string {
 
-	s := lines[0]
-	
-	for {
-		nextS := process(s)
-		
-		if s == nextS {
-			break
-		}
-		
-		s = nextS
+	input := lines[0]
+	if partOne {
+		s := react(input)
+		return fmt.Sprint(len(s))
 	}
-	
 
-	return string(len(s))
+	unitTypes := make(map[rune]bool)
+	messages := make(chan string, 100)
+	jobs := make(chan rune, 100)
+	totalCandidates := 0
+
+	task := func(upper rune) string {
+		lower := unicode.ToLower(upper)
+		newInput := strings.Replace(input, string(upper), "", -1)
+		newCandidate := strings.Replace(newInput, string(lower), "", -1)
+		newCandidate = getNewCandidate(input, upper, lower)
+		stable := react(newCandidate)
+		return stable
+	}
+
+	for w := 1; w <= 4; w++ {
+		go worker(w, jobs, messages, task)
+	}
+
+	for _, r := range []rune(input) {
+		_, found := unitTypes[r]
+		if !found {
+			totalCandidates++
+			upperR := unicode.ToUpper(r)
+			lowerR := unicode.ToLower(r)
+			unitTypes[upperR] = true
+			unitTypes[lowerR] = true
+			jobs <- upperR
+		}
+	}
+	close(jobs)
+
+	min := math.MaxInt64
+	minS := ""
+
+	finished := 0
+	for totalCandidates > finished {
+		// fmt.Printf("Finished %v of %v total %v \n", finished, totalCandidates, float64(finished)/float64(totalCandidates))
+		stable := <-messages
+		if len(stable) < min {
+			min = len(stable)
+			minS = stable
+		}
+		finished++
+	}
+
+	return fmt.Sprint(len(minS))
 }
 
-func process(s string) string {
-	nextS := ""
-	for i := 0; i < (len(s) - 2); i++ {
+func getNewCandidate(input string, upper rune, lower rune) string {
+	s := []rune(input)
+	var next []rune
+	for i := 0; i < len(s); i++ {
+		if s[i] == upper || s[i] == lower {
+			continue
+		}
+		next = append(next, s[i])
+	}
 
+	return string(next)
+}
 
-		if ((unicode.IsLower(s[i]) && unicode.IsUpper(s[i+1])) || (unicode.IsUpper(s[i]) && unicode.isLower(s[i+1]))) {
+func equal(a, b []rune) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
 
+func react(input string) string {
+	s := []rune(input)
+	for {
+		nextS := process(s)
+
+		if equal(nextS, s) {
+			break
 		}
 
-
+		s = nextS
 	}
+	return string(s)
+}
+
+func process(runes []rune) []rune {
+	var next []rune
+	removedLast := false
+	for i := 0; i < (len(runes) - 1); i++ {
+		// compare if two runes have different case i.e. a and A or B and b.
+		if runes[i]^runes[i+1] == 32 {
+			i++
+			removedLast = true
+			continue
+		}
+		removedLast = false
+		next = append(next, runes[i])
+	}
+
+	if !removedLast {
+		next = append(next, runes[len(runes)-1])
+	}
+
+	return next
 }
