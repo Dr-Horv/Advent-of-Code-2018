51cf025ad164059a29f62786cb173204336b557a
Johan Horvius <drhorv@gmail.com>
2018-12-26 04:02:15 +0100

    Solution for day19 part b

--- cmd/aoc.go
+++ cmd/aoc.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/dr-horv/advent-of-code-2018/internal/day24"
+	"github.com/dr-horv/advent-of-code-2018/internal/day19"
 	"github.com/dr-horv/advent-of-code-2018/internal/pkg"
 	"log"
 	"os"
@@ -29,10 +29,10 @@ func main() {
 		defer pprof.StopCPUProfile()
 	}
 
-	day := "24"
+	day := "19"
 	lines := pkg.ReadFile("./internal/day" + day + "/input")
 	start := time.Now()
-	answer := day24.Solve(lines, false)
+	answer := day19.Solve(lines, false)
 	t := time.Now()
 	elapsed := t.Sub(start)
 	fmt.Println(answer)
--- internal/day19/day19.go
+++ internal/day19/day19.go
@@ -3,18 +3,68 @@ package day19
 import (
 	"fmt"
 	"github.com/dr-horv/advent-of-code-2018/internal/pkg"
+	"math"
 	"strings"
 )
 
 type op func(registers []int, a int, b int, c int)
 type inststruction func(registers []int)
 
+type ins struct {
+	instr inststruction
+	index int
+}
+
 func Solve(lines []string, partOne bool) string {
 
+	if partOne {
+		return doPartOne(lines)
+	}
+
+	a := 1 // 0
+	b := 0 // 1
+	c := 0 // 2
+	d := 0 // 3
+	e := 0 // 4
+	f := 0 // 5, program pointer
+
+	f = f + 16
+	f++
+	a, b, c, d, e, f = doLine17(a, b, c, d, e, f)
+	f++
+	if a == 0 {
+		a, b, c, d, e, f = doLine1(a, b, c, d, e, f)
+		return fmt.Sprint(a)
+	} else {
+		d = 27
+		f = 27
+		f++
+		d = d * f
+		f++
+		d = d + f
+		f++
+		d = d * f
+		f++
+		d = d * 14
+		f++
+		d = d * f
+		f++
+		e = e + d
+		a = 0
+		f = 0
+		a, b, c, d, e, f = doLine1(a, b, c, d, e, f)
+		return fmt.Sprint(a)
+
+	}
+
+}
+
+func doPartOne(lines []string) string {
 	parts := strings.Split(lines[0], " ")
 	pointerRegistry := pkg.StrConv(parts[1])
-	program := make([]inststruction, 0)
+	program := make([]ins, 0)
 
+	index := 0
 	for _, l := range lines[1:] {
 		parts := strings.Split(l, " ")
 		instruction := parts[0]
@@ -55,31 +105,57 @@ func Solve(lines []string, partOne bool) string {
 		case "eqrr":
 			f = pkg.Eqrr
 		}
-		fmt.Println(instruction)
 		inst := func(registers []int) { f(registers, a, b, c) }
 		inst(make([]int, 6))
-		program = append(program, inst)
+		program = append(program, ins{inst, index})
+		index++
 	}
 
 	fmt.Println("Parsing done")
 	registers := make([]int, 6)
-
-	registers[0] = 1
-
+	instructionsDone := 0
 	for {
 		nextInstructionIndex := registers[pointerRegistry]
 		if nextInstructionIndex < 0 || nextInstructionIndex >= len(program) {
-			break
+			return fmt.Sprint(registers[0])
 		}
 
-		inst := program[nextInstructionIndex]
-		//fmt.Printf("Before: %v\n", registers)
+		ins := program[nextInstructionIndex]
+		inst := ins.instr
 		inst(registers)
-		//fmt.Printf("After: %v\n", registers)
 		registers[pointerRegistry] = registers[pointerRegistry] + 1
+		instructionsDone++
 	}
+}
 
-	fmt.Printf("After: %v\n", registers)
+func doLine17(a int, b int, c int, d int, e int, f int) (int, int, int, int, int, int) {
+	e = e + 2
+	f++
+	e = e * e
+	f++
+	e = f * e
+	f++
+	e = e * 11
+	f++
+	d = d + 2
+	f++
+	d = d * f
+	f++
+	d = d + 13
+	f++
+	e = e + d
+	f++
+	return a, b, c, d, e, f
+}
 
-	return fmt.Sprint(registers[0])
+func doLine1(a int, b int, c int, d int, e int, f int) (int, int, int, int, int, int) {
+	c = 1
+	a = a + 1 + e
+	for i := 2; i <= int(math.Sqrt(float64(e))); i++ {
+		if e%i == 0 {
+			a += i
+			a += e / i
+		}
+	}
+	return a, b, c, d, e, f
 }
